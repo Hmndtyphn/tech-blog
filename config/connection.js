9f136560ec4d7167c984ab94cebd0360abc65080
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-04 10:50:14 -0600

    connection

--- config/connection.js
+++ config/connection.js
@@ -6,3 +6,16 @@ require("dotenv").config();
 
 // connection to db
 const sequelize = process.env.JAWSDB_URL
+
+// connect to localhost/ use mysql2
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
+
+    // export Sequelize
+    module.exports = sequelize;
