1baa00e07fd10a6fd5b62395ab2237d8a35c47af
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-06 10:43:52 -0600

    server setup

--- server.js
+++ server.js
@@ -2,11 +2,49 @@
 const express = require("express");
 const path = require("path");
 
+// require handlebars
 const exphbs = require("express-handlebars");
 
-const sessions = require("express-session");
-// const controller = require("./controllers");
-// const sequlieze = require("./config/connection");
+// require express session
+const session = require("express-session");
 
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// paths for config/ controllers
+const controller = require("./controllers");
+const sequelize = require("./config/connection");
 
+// connect session sqlize store
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// session function
+const exSession = {
+    secret: "super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore ({
+        db: sequelize,
+    }),
+};
+
+// intializes server on port 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express/ exp session middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, "public")));
+app.use(session(exSession));
+
+// allows use of controllers
+app.use("/", controller);
+
+// setup/ use handlebars
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+
+// sync sequelize
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
