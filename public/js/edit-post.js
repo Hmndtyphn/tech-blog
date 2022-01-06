c82d1c12c545301cf4df13e049bd33e3f73894f0
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-06 11:38:28 -0600

    added post deletion

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,4 +1,4 @@
-// handles edit of post
+// edit a post
 async  function editPost(event) {
     event.preventDefault();
 
@@ -15,4 +15,41 @@ async  function editPost(event) {
     // replace post id/ edit
     document.location.replace("/edit/" + post_id);
 }
+
+// delete a post
+async function deletePost(event) {
+    event.preventDefault();
+
+    // post delete request
+    const post_id = window.location.toString().split("/")[
+
+        // reduces post by 1
+        window.location.toString().split("/").length - 1
+    ];
+
+    // awaits post, then deletes content
+    const response = await fetch("/api/posts/" + post_id, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    // checks for response 
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        // alert response 
+        alert(response.statusText);
+    }
+}
+
+//  edit current post on click event listener
+document.querySelector("#edit-btn").addEventListener("click", editPost);
+
+// delete current post on click event listener
+document
+    .querySelector("#delete-btn")
+    .addEventListener("click", deletePost);
+    
 
