30349a19e16afcf072d59ed7ae138b7e4ca0620a
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-06 10:53:17 -0600

    added inactivity timer

---
+++ public/js/home.js
@@ -0,0 +1,27 @@
+// inactivity timer
+const inactiveTimer = function () {
+    const time;
+    window.onload = resetTimer
+
+    // events 
+    document.onkeypress = resetTimer;
+    document.onmousemove = resetTimer;
+
+    // logout due to inactivity
+    function logoutUser() {
+        logout();
+    }
+
+    // reset timer
+    function resetTimer() {
+        clearTimeout(time);
+
+        // logout after 30 seconds
+        time = setTimeout(logoutUser, 30000);
+    }
+};
+
+// run inactivity timer on current window
+window.onload = function () {
+    inactiveTimer();
+};
