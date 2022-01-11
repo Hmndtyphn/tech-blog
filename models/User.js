cc8eec419fdeaa2fee941a13a09215dfddada969
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-11 15:22:30 -0600

    user model

--- models/User.js
+++ models/User.js
@@ -39,14 +39,60 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: { len: [4] },
+            validate: { isEmail: true },
+            unique: true,
+        },
+
+        // password data
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [8] },
+
         },
     },
 
     {
         hooks: {
-            
-        }
+
+            // async for user creation
+            async beforeCreate(newUserData) {
+
+                // await bcrypt pw hhash
+                newUserData.password = await bcrypt.hash(newUserData.password, 8);
+
+                // return new user
+                return newUserData;
+            },
+
+            // updating
+            async beforeUpdate(updateUserData) {
+
+                // update pw
+                updateUserData.password = await bcrypt.hash(
+
+                    // updated
+                    updateUserData.password,
+                    8
+                );
+
+                // returns updated data
+                return updateUserData;
+            },
+        },
+        sequlize,
+
+        // modelName
+        modelName: "user",
+
+        // stops db from changing name
+        freezeTableName: true,
+
+        // underscores> camelCase
+        underscored: true,
+
+        // create timestamps for data
+        timestamps: true,
     }
-)
+);
 
