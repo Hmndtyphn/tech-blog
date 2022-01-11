ee3397e228dd05a225abc044e8776c24e5a79073
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-11 15:48:01 -0600

    model associations

--- models/index.js
+++ models/index.js
@@ -1,2 +1,44 @@
 // imports from models folder
 const User = require("./User");
+
+// import post
+const Post = require("./Post");
+
+// import comment
+const Comment = require("./Comment");
+
+
+
+// user relation 
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
+// post belongs to user id
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// user comment id
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+
+// comment belongs to user id
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// post id comment
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+});
+
+
+// exports users, comments and posts
+module.exports = { User, Comment, Post };
