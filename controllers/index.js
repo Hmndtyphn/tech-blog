ae2cceb8c030ffa4943305029802af7bbff440e8
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-11 18:51:25 -0600

    controllers configured

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,28 @@ const router = require("express").Router();
 // requires home routes
 const homeRoutes = require("./home-routes");
 
+// require user routes
+const userRoutes = require("./user-routes");
+
+// require post routes
+const postRoutes = require("./post-routes");
+
+// require comment routes
+const commentRoutes = require("./comment-routes");
 
 
 
 // use router
 router.use("/", homeRoutes);
+
+// use user routes
+router.use("/", userRoutes);
+
+// use post routes
+router.use("/", postRoutes);
+
+// use comment routes
+router.use("/", commentRoutes);
 
 
 // export router
